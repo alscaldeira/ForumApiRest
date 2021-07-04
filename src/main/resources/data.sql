22e84a4d0970700b19ab7f6c583d7e4eec4da71e
alscaldeira <anderson0caldeira@gmail.com>
2021-07-04 18:40:06 -0300

    Added security.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$5Z157tH7pbvOZLifF8jPau939xVK.Vezydjds.pot9HWqXl8fDFsa');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
